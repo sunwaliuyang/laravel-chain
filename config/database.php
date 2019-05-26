eb858e0c37bbb70500494294eea8f0834f9603dc
ly <571666271@qq.com>
2019-05-26 14:27:39 +0000

    heroku PostgreSQL数据库配置

--- config/database.php
+++ config/database.php
@@ -1,8 +1,10 @@
 <?php
 
 use Illuminate\Support\Str;
-
+$db_config = get_db_config();
 return [
+
+
 
     /*
     |--------------------------------------------------------------------------
@@ -32,7 +34,7 @@ return [
     | choice installed on your machine before you begin development.
     |
     */
-
+    'default' => $db_config['connection'],
     'connections' => [
 
         'sqlite' => [
@@ -66,11 +68,11 @@ return [
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => $db_config['host'],
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => $db_config['database'],
+            'username' => $db_config['username'],
+            'password' => $db_config['password'],
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
