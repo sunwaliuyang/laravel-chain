a9b0460f5d32121b0fb3b412efa671f4b3109313
ly <571666271@qq.com>
2019-05-26 05:04:23 +0000

    首页基本样式布局

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,5 +1,17 @@
 @extends('layouts.default')
-@section('title', '主页面')
 @section('content')
-    <h1>主页</h1>
+    <div class="jumbotron">
+        <h1>Hello Laravel</h1>
+        <p class="lead">
+            你现在所看到的是
+            <a href="https://learnku.com/courses/laravel-essential-training/5.8/style-beautification/4067" >laravel 入门教程</a>
+            的示例项目首页
+        </p>
+        <p>
+            一切，将从这里开始。
+        </p>
+        <p>
+            <a class="btn btn-lg btn-success" href="#" role="button">现在注册</a>
+        </p>
+    </div>
 @stop
